9b2a2690f6da8c180fd46f75bd1c8762dd32633e
Benj Boyer <15024379+benjboyer@users.noreply.github.com>
2020-07-30 12:39:23 -0400

    * Fixed unit tests * Added Closure simulation for testing (#84) * Refactored defaults scripts to inv-repo

---
+++ tools/maven/test.groovy
@@ -0,0 +1,35 @@
+@Test
+void mavenSimpleLookup() {
+    def app1 = new File("./testResources/SimpleMavenLookup/app1").absolutePath
+    def app2 = new File("./testResources/SimpleMavenLookup/app2").absolutePath
+
+    simulate(
+            "inv.groovy",
+            "../../io/files/inv.groovy",
+            {
+                name "app1"
+                path app1
+
+                // Using default
+                require $inv.Maven into '$maven'
+
+                step {
+                    assert $maven.poms
+                }
+            },
+            {
+                name "app2"
+
+                require $inv.Maven using {
+
+                    // Disabling defaults and calling manually
+                    defaults false
+
+                    resolved {
+                        response.analyze(app2)
+                    }
+                }
+            }
+    )
+    assertTrue isOk
+}
