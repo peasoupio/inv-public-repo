47a4b29ecdb501ea6764e5e6f010ae84e3cab43a
benjboyer <15024379+benjboyer@users.noreply.github.com>
2020-12-23 21:58:00 -0500

    * Added os/exec * Added tools/docker * Removed unecessary addRepoFile from maven tests

---
+++ tools/docker/vars/inv.groovy
@@ -0,0 +1,34 @@
+
+get "https://raw.githubusercontent.com/peasoupio/inv-public-repo/master/os/exec/repo.yml"
+
+inv {
+
+    name "docker"
+
+    require { Exec }
+
+    broadcast { Docker } using {
+        ready {[
+                exists: { String imageName ->
+                    assert imageName, "Image name is required"
+
+                    String returnValue = $exec.params(
+                            returnStdout: true,
+                            script: "docker images -q ${imageName}",
+                            basedir: pwd)
+
+                    return !returnValue.isEmpty()
+                },
+                build: { File dockerFile, String imageName ->
+                    assert dockerFile, 'Dockerfile is required'
+                    assert dockerFile.exists(), 'Dockerfile must be present on filesystem'
+
+                    return $exec.params(
+                            returnStatus: true,
+                            script: "docker build -t ${imageName} ./",
+                            basedir: dockerFile.parent) == "0"
+                }
+        ]}
+    }
+
+}
--- tools/maven/test/test.groovy
+++ tools/maven/test/test.groovy
@@ -9,7 +9,6 @@ void mavenSimpleLookup() {
     def app2 = new File("./resources/test/SimpleMavenLookup/app2").absolutePath
 
     simulate {
-        addRepoFile "../../io/files"
         addInvFile "vars/inv.groovy"
 
         addInvBody {
